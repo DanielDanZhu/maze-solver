ff5f0e4ddb6574a52e6039cbee90bd95b62f6a7c
Daniel Zhu <dzdude77@gmail.com>
2018-09-28 08:39:20 -0400

    Start/end info contained in maze instead of node

--- mazeSolver.py
+++ mazeSolver.py
@@ -6,6 +6,7 @@ class Maze:
         self.pix = self.im.load()
         self.pixels = []
         self.nodes = []
+        self.start_node = self.end_node = None
 
     def in_range(self, x, y):
         return x >= 0 and x < self.im.width and y>= 0 and y < self.im.height
@@ -24,18 +25,20 @@ class Maze:
             for x in range(self.im.width):
                 if not self.pixels[x][y]:                                       # is path
                     if not self.in_range(x, y-1):                               # start
-                        self.nodes.append(Node(x, y, [], True, False))
+                        self.nodes.append(Node(x, y, []))
+                        self.start_node = self.nodes[len(self.nodes)-1]
                     elif not self.in_range(x, y+1):                             # end
-                        self.nodes.append(Node(x, y, [], False, True))
+                        self.nodes.append(Node(x, y, []))
+                        self.end_node = self.nodes[len(self.nodes)-1]
                     elif (self.pixels[x-1][y] != self.pixels[x+1][y] or         # left != right
                           self.pixels[x][y-1] != self.pixels[x][y+1]):          # down != up
-                        self.nodes.append(Node(x, y, [], False, False))
+                        self.nodes.append(Node(x, y, []))
 
     def connect_nodes(self):
         directions = [[1, 0], [0, 1]]
 
         for node in self.nodes:
-            if not node.e:
+            if node != self.end_node:
                 for xdir, ydir in directions:                                   # checks right and down
                     currx = node.x + xdir
                     curry = node.y + ydir
@@ -62,18 +65,17 @@ class Maze:
 
     def display_nodes(self):
         for i in self.nodes:
-            print(str(i.x) + ", " + str(i.y) + ": " + str(len(i.connections)) + " " + str(i.s) + " " + str(i.e))
-
-    def display_nodes_distance(self):
-        for i in self.nodes:
             print("(" + str(i.x) + ", " + str(i.y) + "): ", end = '')
             for other in i.connections:
                 print("[(" + str(other.x) + ", " + str(other.y) + "), " + str(i.get_distance(other)) + "] ", end = '')
             print()
 
+    def dijkstras(self):
+        
+
 class Node:
-    def __init__(self, x, y, connections, s, e):
-        self.x, self.y, self.connections, self.s, self.e = x, y, connections, s, e
+    def __init__(self, x, y, connections):
+        self.x, self.y, self.connections= x, y, connections
 
     def add_connection(self, other_node):
         self.connections.append(other_node)
@@ -83,10 +85,9 @@ class Node:
             return 0
         return abs(self.x - other_node.x) + abs(self.y - other_node.y)
 
-m = Maze('maze.png')
+m = Maze('maze4.png')
 m.convert()
 m.display_maze()
 m.create_nodes()
 m.connect_nodes()
 m.display_nodes()
-m.display_nodes_distance()
